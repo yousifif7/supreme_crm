3d5854481a81dc6a0a4f2d336f2bde75349e749b
yousif <yousiftheking2001@gmail.com>
2025-08-07 01:09:42 +0500

    New User Changes

--- app/Models/TrainingAcknowledgement.php
+++ app/Models/TrainingAcknowledgement.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class TrainingAcknowledgement extends Model
 {
-    //
+    
+    protected $fillable = [
+    'user_id',
+    'training_material_id',
+    'acknowledged_at',
+    'completion_time_seconds',
+];
 }
--- app/Models/TrainingMaterial.php
+++ app/Models/TrainingMaterial.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class TrainingMaterial extends Model
 {
-    //
+    
+
+    protected $fillable = [
+    'title',
+    'type',
+    'content_url',
+    'pdf_url',
+    'required',
+    'expiry_date',
+];
+
+
+    public function acknowledgements(){
+  
+     return $this->hasMany(TrainingAcknowledgement::class,'training_material_id');
+
+    }
 }
