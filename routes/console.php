c2c99679ba5bdff4abf3a21b6eee5d7893647f97
yousif <yousiftheking2001@gmail.com>
2025-08-16 03:30:55 +0500

    New Chanes

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::command('notify:document-expiry')->everyMinute();
+Schedule::command('shifts:process-notifications')->everyMinute();
+Schedule::command('notify:shifts')->everyMinute();
