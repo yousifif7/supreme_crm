dbc6362c43e9de8f2b0577f9c4fff0cc99290301
yousif <yousiftheking2001@gmail.com>
2025-09-08 16:48:19 +0300

    export leaves

--- app/Exports/LeavesExport.php
+++ app/Exports/LeavesExport.php
@@ -2,7 +2,6 @@
 
 namespace App\Exports;
 
-use App\Models\EmployeeLeave;
 use App\Models\LeaveRequest;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Illuminate\Support\Facades\DB;
@@ -12,35 +11,30 @@ class LeavesExport implements FromCollection, WithHeadings
 {
     public function collection()
     {
-        $leaves = LeaveRequest::select(
-            DB::raw("CONCAT(employees.fore_name, ' ', employees.sur_name) as full_name"),
-            'leave_requests.employee_id',
-            'leave_requests.leave_entitlement as details',
+        return LeaveRequest::select(
+            DB::raw("CONCAT(users.first_name, ' ', users.last_name) as employee"),
+            'leave_requests.reason as details',
             'leave_requests.type as leave_type',
-            'leave_requests.from_date',
-            'leave_requests.to_date',
+            'leave_requests.start_date as date_from',
+            'leave_requests.end_date as date_to',
             'leave_requests.status',
             'leave_requests.reject_reason',
-            'leave_requests.hours',
+            'leave_requests.hours as hours_requested',
             'leave_requests.approved_hours',
             'leave_requests.paid',
             'leave_requests.ssp_paid_days',
             'leave_requests.unpaid_days',
             'leave_requests.amount_paid',
-            DB::raw("DATE_FORMAT(leave_requests.created_at, '%Y-%m-%d %H:%i') as applied_at"),
-            DB::raw("DATE_FORMAT(leave_requests.approved_at, '%Y-%m-%d %H:%i') as approved_at")
+            DB::raw("DATE_FORMAT(leave_requests.created_at, '%Y-%m-%d') as applied_at")
         )
-        ->join('employees', 'employees.id', '=', 'leave_requests.employee_id')
+        ->join('users', 'users.id', '=', 'leave_requests.user_id')
         ->get();
-
-        return $leaves;
     }
 
     public function headings(): array
     {
         return [
             'Employee',
-            'Employee ID',
             'Details',
             'Leave Type',
             'Date From',
@@ -54,7 +48,6 @@ class LeavesExport implements FromCollection, WithHeadings
             'Unpaid Days',
             'Amount Paid',
             'Applied At',
-            'Approved At',
         ];
     }
 }
